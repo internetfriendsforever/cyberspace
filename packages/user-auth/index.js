3519e186b056ef6686df86ba09e8037064ed46b3
Daniel Mahal <danielmahal@gmail.com>
2018-09-18 19:04:23 +0200

    Expose redirectSuccess/Error from user-auth

--- packages/user-auth/index.js
+++ packages/user-auth/index.js
@@ -4,5 +4,7 @@ module.exports = {
   login: require('./src/middleware/login'),
   logout: require('./src/middleware/logout'),
   requestToken: require('./src/middleware/requestToken'),
-  validateToken: require('./src/middleware/validateToken')
+  validateToken: require('./src/middleware/validateToken'),
+  redirectError: require('./src/middleware/redirectError'),
+  redirectSuccess: require('./src/middleware/redirectSuccess')
 }
