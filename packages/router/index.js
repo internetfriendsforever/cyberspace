ac524e61a023e8012293dee179e8a125b75ce1a3
Daniel Mahal <danielmahal@gmail.com>
2018-11-28 15:26:33 +0100

    Fix bug in router resolve where it returns null instead of object

--- packages/router/index.js
+++ packages/router/index.js
@@ -1,35 +1,30 @@
-var pathToRegexp = require('path-to-regexp')
+const pathToRegexp = require('path-to-regexp')
 
 module.exports = {
   resolve: function (routes, rawPath) {
-    var keys = []
+    const params = {}
+    const path = normalize(rawPath)
 
-    var path = normalize(rawPath)
-
-    var key = Object.keys(routes).find(function (pattern) {
+    const key = Object.keys(routes).find(function (pattern) {
       return pathToRegexp(pattern).test(path)
     })
 
-    var route = routes[key] || routes['404']
+    const route = routes[key]
 
-    if (route) {
-      var params = {}
+    if (key) {
+      const paramKeys = []
 
-      if (key) {
-        var values = pathToRegexp(key, keys).exec(path).slice(1)
+      const values = pathToRegexp(key, paramKeys).exec(path).slice(1)
 
-        keys.forEach(function (key, i) {
-          params[key.name] = values[i]
-        })
-      }
-
-      return {
-        key: key,
-        params: params
-      }
+      paramKeys.forEach(function (key, i) {
+        params[key.name] = values[i]
+      })
     }
 
-    return null
+    return {
+      key: key,
+      params: params
+    }
   },
 
   listen: function (handler, {
@@ -39,7 +34,7 @@ module.exports = {
     scroll = true
   } = {}) {
     function navigate (path, options = {}) {
-      var fn = options.replace ? 'replaceState' : 'pushState'
+      const fn = options.replace ? 'replaceState' : 'pushState'
 
       window.history[fn](null, null, path)
 
@@ -62,22 +57,22 @@ module.exports = {
 
     if (click) {
       window.addEventListener('click', function (e) {
-        var link = e.target.closest('a')
-        var ignore = e.ctrlKey || e.shiftKey || e.altKey || e.metaKey
+        const link = e.target.closest('a')
+        const ignore = e.ctrlKey || e.shiftKey || e.altKey || e.metaKey
 
         if (!ignore && link) {
           e.preventDefault()
 
-          var domain = function (url) {
+          const domain = function (url) {
             return url.replace('http://', '').replace('https://', '').split('/')[0]
           }
 
-          var external = domain(window.location.href) !== domain(link.href)
+          const external = domain(window.location.href) !== domain(link.href)
 
           if (!external) {
-            var pathname = link.pathname
-            var search = link.search || ''
-            var hash = link.hash || ''
+            const pathname = link.pathname
+            const search = link.search || ''
+            const hash = link.hash || ''
             navigate([pathname, search, hash].join(''))
           } else {
             window.open(link.href)
