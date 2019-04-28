07b4a25c79e9d8841d01a75520e34867f0523c9a
Daniel Mahal <danielmahal@gmail.com>
2019-04-28 10:15:32 +0200

    Use portfinder to find open port in server

--- packages/example-sanity-site/src/server.js
+++ packages/example-sanity-site/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import portfinder from 'portfinder'
 import bundle from '@cyberspace/webpack-config/bundle'
 import router from '@cyberspace/router'
 import createApiClient from '@cyberspace/api-client'
@@ -8,55 +9,62 @@ import apiMiddleware from './api'
 import routes from './routes'
 import favicon from './assets/favicon.png'
 
-const app = express()
-
-const port = 3001
-const url = process.env.NOW_URL || `http://localhost:${port}`
-const apiPath = '/api'
-const apiUrl = `${url}${apiPath}`
-const apiClient = createApiClient(apiUrl)()
-
-app.use('/static', bundle)
-
-app.use(apiPath, apiMiddleware)
-
-app.use('/invalidate', (req, res) => {
-  apiClient.flush()
-  res.status(200).end()
-})
-
-app.use(async (req, res) => {
-  try {
-    const { path, query } = req
-    const { key, params } = router.resolve(routes, path)
-    const navigate = path => res.redirect(path)
-    const route = await routes[key || '404']({ path, params, query, navigate, api: apiClient })
-
-    if (!res.headersSent) {
-      res.status(route.statusCode || 200).send(`
-        <!doctype html>
-        <html lang='no'>
-          <head>
-            <title>${route.title}</title>
-            <meta charset='utf-8' />
-            <meta name='viewport' content='width=device-width, initial-scale=1' />
-            <meta name='theme-color' content='#000' />
-            <link rel='icon' type='image/png' href='${favicon}'>
-            <script src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
-          </head>
-          <body>
-            <div id='root'>${renderStylesToString(renderToString(route.component))}</div>
-            <script>window.dehydrated = ${apiClient.dehydrate()};</script>
-            <script src='/static/client.js'></script>
-          </body>
-        </html>
-      `)
-    }
-  } catch (error) {
-    res.status(500).send(`Server error: ${error}`)
+portfinder.getPort({
+  port: 3000,
+  stopPort: 3333
+}, (error, port) => {
+  if (error) {
+    throw error
   }
-})
 
-app.listen(port, () => {
-  console.log('Server listening at port', port)
+  const app = express()
+  const url = process.env.NOW_URL || `http://localhost:${port}`
+  const apiPath = '/api'
+  const apiUrl = `${url}${apiPath}`
+  const apiClient = createApiClient(apiUrl)()
+
+  app.use('/static', bundle)
+
+  app.use(apiPath, apiMiddleware)
+
+  app.use('/invalidate', (req, res) => {
+    apiClient.flush()
+    res.status(200).end()
+  })
+
+  app.use(async (req, res) => {
+    try {
+      const { path, query } = req
+      const { key, params } = router.resolve(routes, path)
+      const navigate = path => res.redirect(path)
+      const route = await routes[key || '404']({ path, params, query, navigate, api: apiClient })
+
+      if (!res.headersSent) {
+        res.status(route.statusCode || 200).send(`
+          <!doctype html>
+          <html lang='no'>
+            <head>
+              <title>${route.title}</title>
+              <meta charset='utf-8' />
+              <meta name='viewport' content='width=device-width, initial-scale=1' />
+              <meta name='theme-color' content='#000' />
+              <link rel='icon' type='image/png' href='${favicon}'>
+              <script src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
+            </head>
+            <body>
+              <div id='root'>${renderStylesToString(renderToString(route.component))}</div>
+              <script>window.dehydrated = ${apiClient.dehydrate()};</script>
+              <script src='/static/client.js'></script>
+            </body>
+          </html>
+        `)
+      }
+    } catch (error) {
+      res.status(500).send(`Server error: ${error}`)
+    }
+  })
+
+  app.listen(port, () => {
+    console.log('Server listening at port', port)
+  })
 })
--- packages/example-simple-site/src/server.js
+++ packages/example-simple-site/src/server.js
@@ -7,41 +7,6 @@ import { renderStylesToString } from 'emotion-server'
 import routes from './routes'
 import favicon from './assets/favicon.png'
 
-const app = express()
-
-app.use('/static', bundle)
-
-app.use(async (req, res) => {
-  try {
-    const { path, query } = req
-    const { key, params } = router.resolve(routes, path)
-    const navigate = path => res.redirect(path)
-    const route = await routes[key || '404']({ path, params, query, navigate })
-
-    if (!res.headersSent) {
-      res.status(route.statusCode || 200).send(`
-        <!doctype html>
-        <html lang='no'>
-          <head>
-            <title>${route.title}</title>
-            <meta charset='utf-8' />
-            <meta name='viewport' content='width=device-width, initial-scale=1' />
-            <meta name='theme-color' content='#000' />
-            <link rel='icon' type='image/png' href='${favicon}'>
-            <script src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
-          </head>
-          <body>
-            <div id='root'>${renderStylesToString(renderToString(route.component))}</div>
-            <script src='/static/client.js'></script>
-          </body>
-        </html>
-      `)
-    }
-  } catch (error) {
-    res.status(500).send(`Server error: ${error}`)
-  }
-})
-
 portfinder.getPort({
   port: 3000,
   stopPort: 3333
@@ -49,6 +14,41 @@ portfinder.getPort({
   if (error) {
     throw error
   }
+
+  const app = express()
+
+  app.use('/static', bundle)
+
+  app.use(async (req, res) => {
+    try {
+      const { path, query } = req
+      const { key, params } = router.resolve(routes, path)
+      const navigate = path => res.redirect(path)
+      const route = await routes[key || '404']({ path, params, query, navigate })
+
+      if (!res.headersSent) {
+        res.status(route.statusCode || 200).send(`
+          <!doctype html>
+          <html lang='no'>
+            <head>
+              <title>${route.title}</title>
+              <meta charset='utf-8' />
+              <meta name='viewport' content='width=device-width, initial-scale=1' />
+              <meta name='theme-color' content='#000' />
+              <link rel='icon' type='image/png' href='${favicon}'>
+              <script src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
+            </head>
+            <body>
+              <div id='root'>${renderStylesToString(renderToString(route.component))}</div>
+              <script src='/static/client.js'></script>
+            </body>
+          </html>
+        `)
+      }
+    } catch (error) {
+      res.status(500).send(`Server error: ${error}`)
+    }
+  })
 
   app.listen(port, () => {
     console.log('Server listening at port', port)
