39f83a22884ff15474548b658ff7bf42ba39d32a
Daniel Mahal <danielmahal@Daniels-MacBook-Pro.local>
2019-06-22 22:53:32 +0200

    Change static path, add binary media type to run template

--- packages/example-run/index.js
+++ packages/example-run/index.js
@@ -2,14 +2,13 @@ const path = require('path')
 const router = require('@cyberspace/run-router')
 const file = require('@cyberspace/run-file')
 
-const staticPath = path.resolve(__dirname, `src/static/`)
+const staticPath = path.join(__dirname, `src/static`)
 const staticHandler = ({ params }) => file(path.join(staticPath, params[0]))
 
 exports.handler = router({
   GET: {
     '/': require('./src/home.js'),
     '/about': require('./src/about.js'),
-    '/static/(.*)': staticHandler,
-    '(.*)': require('./src/404.js')
+    '/static/(.*)': staticHandler
   }
 })
