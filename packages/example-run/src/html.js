901e6dfbffbdac1e2d0b4300c731100597b77d2b
Daniel Mahal <danielmahal@gmail.com>
2019-08-04 21:28:23 +0200

    Add utf-8 meta tag to run example

--- packages/example-run/src/html.js
+++ packages/example-run/src/html.js
@@ -17,6 +17,7 @@ module.exports = ({ statusCode = 200, title, body }) => ({
     <html>
       <head>
         <title>${title}</title>
+        <meta charset="utf-8" />
         <link rel="shortcut icon" href="/static/favicon.png" />
         <link rel="stylesheet" href="/styles.css" />
       </head>
